733941b182f427414d483cae9e4dcb27004b4ff5
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-14 18:38:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
