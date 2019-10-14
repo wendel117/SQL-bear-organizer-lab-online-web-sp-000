1132b0fdf42ba6f043ef6e9ae9ad3a5d4b235178
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-14 17:31:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (Mr. Chocolate, age, gender, color, temperament, alive) VALUES ('Maru', 3, 'Scottish Fold');
