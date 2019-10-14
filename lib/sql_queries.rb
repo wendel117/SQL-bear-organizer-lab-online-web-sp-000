7c4268b77c378b6fc9ae8fad5b63aa96acfe02b7
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-14 18:27:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,9 +1,9 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender == F"
+  "SELECT name, age FROM bears WHERE gender=F"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT name FROM bears GROUP"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  ""
 end
 
 def selects_most_prominent_color_and_returns_with_count
